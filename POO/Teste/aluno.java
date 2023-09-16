cea5c81bab05cbbd5b16bce7710c665f53b0e26d
Victor Querino Martins <123746753+VictorQMm@users.noreply.github.com>
2023-09-16 18:47:58 -0300

    Add files via upload

---
+++ POO/Teste/aluno.java
@@ -0,0 +1,23 @@
+package JAVA.POO.Teste;
+
+public class aluno {
+
+    String nome;
+    int idade;
+    String curso;
+    
+    void estudar(){                         //()<-- parâmetro    // void pois n tem retorno
+        System.out.println(this.nome + " esta estudando ");
+    }
+}
+
+
+
+
+
+
+
+
+
+
+// Integer.parseInt --> Converte string para inteiro.
