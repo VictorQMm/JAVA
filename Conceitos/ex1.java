d2566b0d6fa9c4d1e18c6d17c5e420f57f824e6f
Victor Querino Martins <123746753+VictorQMm@users.noreply.github.com>
2023-09-16 18:48:25 -0300

    Add files via upload

---
+++ Conceitos/ex1.java
@@ -0,0 +1,21 @@
+package JAVA.Conceitos;
+import java.util.Scanner;
+
+import JAVA.Conceitos.sout;
+
+public class ex1{
+    public static void main(String args[]){
+        Scanner s = new Scanner(System.in);
+
+    int soma = 2 + 2;
+    int subtracao = 2 - 2;
+    int multiplicacao = 2 * 2;
+    int divisao = 5/2;
+
+    }
+}
+
+// and  &&                      or                                      not
+// vdd vdd = vdd                false false = false                     vdd = false
+//                                                                       false = vdd
+
---
+++ Conceitos/sout.java
@@ -0,0 +1,9 @@
+package JAVA.Conceitos;
+
+public class sout {
+    public static void main (String[] args){
+        System.out.println();
+        // "sout" é uma forma resumida de dar "System.out.println();""
+    
+}
+}
